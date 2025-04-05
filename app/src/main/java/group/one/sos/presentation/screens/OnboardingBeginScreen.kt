d7bbfcc0d867935c7743040cf431ac9295181aa2
= <ssh.xero@gmail.com>
2025-04-06 00:25:31 +0100

    feat(ui): Prototype initial onboarding screen

--- app/src/main/java/group/one/sos/presentation/screens/OnboardingBeginScreen.kt
+++ app/src/main/java/group/one/sos/presentation/screens/OnboardingBeginScreen.kt
@@ -1,15 +1,27 @@
 package group.one.sos.presentation.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import group.one.sos.R
 import group.one.sos.presentation.theme.SOSTheme
 
@@ -23,9 +35,33 @@ fun OnboardingBeginScreen(
             modifier = modifier
                 .fillMaxSize()
                 .padding(innerPadding)
+                .padding(12.dp)
         ) {
-            Column {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.logo),
+                    contentDescription = null,
+                    modifier = Modifier.size(120.dp)
+                )
+                Spacer(modifier = Modifier.height(24.dp))
                 Text(text = stringResource(R.string.onboarding_text))
+                Spacer(modifier = Modifier.height(24.dp))
+                Button(
+                    onClick = { /* TODO: Move to next stage */ },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(8.dp)
+                ) {
+                    Text(text = stringResource(R.string.get_started_btn))
+                    Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+                    Icon(
+                        painter = painterResource(R.drawable.ic_right_arrow),
+                        contentDescription = null,
+                        modifier = Modifier.size(ButtonDefaults.IconSize)
+                    )
+                }
             }
         }
     }
