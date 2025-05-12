f25ed8ec8c0a77b244022a75fb40866712389096
= <ssh.xero@gmail.com>
2025-05-12 22:41:56 +0100

    feat(ui): Add top bars to various screens

--- app/src/main/java/group/one/sos/presentation/screens/contacts/ContactsScreen.kt
+++ app/src/main/java/group/one/sos/presentation/screens/contacts/ContactsScreen.kt
@@ -1,17 +1,27 @@
 package group.one.sos.presentation.screens.contacts
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import group.one.sos.R
 import group.one.sos.presentation.components.BottomNavBar
 import group.one.sos.presentation.navigation.NavigationRoute
 
 @Composable
 fun ContactsScreen(modifier: Modifier = Modifier, navController: NavController) {
     Scaffold(
+        topBar = { ContactsScreenTopBar() },
         bottomBar = {
             BottomNavBar(
                 navController = navController,
@@ -21,5 +31,21 @@ fun ContactsScreen(modifier: Modifier = Modifier, navController: NavController) 
     ) { innerPadding ->
         Column(modifier = modifier.padding(innerPadding)) {
         }
+    }
+}
+
+@Composable
+private fun ContactsScreenTopBar(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(12.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = stringResource(R.string.contacts_header),
+            style = MaterialTheme.typography.titleMedium
+        )
     }
 }
--- app/src/main/java/group/one/sos/presentation/screens/settings/SettingsScreen.kt
+++ app/src/main/java/group/one/sos/presentation/screens/settings/SettingsScreen.kt
@@ -1,17 +1,27 @@
 package group.one.sos.presentation.screens.settings
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import group.one.sos.R
 import group.one.sos.presentation.components.BottomNavBar
 import group.one.sos.presentation.navigation.NavigationRoute
 
 @Composable
 fun SettingsScreen(modifier: Modifier = Modifier, navController: NavController) {
     Scaffold(
+        topBar = { SettingsScreenTopBar() },
         bottomBar = {
             BottomNavBar(
                 navController = navController,
@@ -21,5 +31,21 @@ fun SettingsScreen(modifier: Modifier = Modifier, navController: NavController) 
     ) { innerPadding ->
         Column(modifier = modifier.padding(innerPadding)) {
         }
+    }
+}
+
+@Composable
+private fun SettingsScreenTopBar(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(12.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = stringResource(R.string.settings_header),
+            style = MaterialTheme.typography.titleMedium
+        )
     }
 }
