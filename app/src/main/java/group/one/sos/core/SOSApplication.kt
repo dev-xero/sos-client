54b02e40f410f11947a69005f80134f41013c999
= <ssh.xero@gmail.com>
2025-05-20 10:23:37 +0100

    feat: Implement reports screen view model

--- app/src/main/java/group/one/sos/core/SOSApplication.kt
+++ app/src/main/java/group/one/sos/core/SOSApplication.kt
@@ -1,7 +1,6 @@
 package group.one.sos.core
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class SOSApplication : Application()
--- app/src/main/java/group/one/sos/presentation/screens/reports/ReportsViewModel.kt
+++ app/src/main/java/group/one/sos/presentation/screens/reports/ReportsViewModel.kt
@@ -3,14 +3,19 @@ package group.one.sos.presentation.screens.reports
 import android.location.Location
 import android.os.Looper
 import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.Priority
 import group.one.sos.core.constants.Tag
+import group.one.sos.domain.contracts.EmergencyRepository
+import group.one.sos.domain.models.IncidentResponse
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 sealed class UiState {
@@ -20,15 +25,22 @@ sealed class UiState {
 
 class ReportsViewModel @Inject constructor(
     private val fusedLocationProviderClient: FusedLocationProviderClient,
-) {
+    private val emergencyRepository: EmergencyRepository,
+) : ViewModel() {
     private val _locationFlow = MutableStateFlow<Location?>(null)
 
     private var hasSetBaseState = false;
 
     private var locationCallBack: LocationCallback? = null
 
+    private val _reports = MutableStateFlow<List<IncidentResponse>>(emptyList())
+    val reports: StateFlow<List<IncidentResponse>> = _reports
+
     private val _uiState = MutableStateFlow<UiState>(UiState.Fetching)
     val uiState: StateFlow<UiState> = _uiState
+
+    private val _error = MutableStateFlow<String?>(null)
+    val error: StateFlow<String?> = _error
 
     init {
         startLocationUpdates()
@@ -63,7 +75,34 @@ class ReportsViewModel @Inject constructor(
         }
     }
 
-    private fun getRecentIncidents() {
+    fun getRecentIncidents() {
+        if (_locationFlow.value != null) {
+            viewModelScope.launch {
+                _uiState.value = UiState.Fetching
+                _error.value = null
 
+                emergencyRepository.getIncidents(
+                    radius = 200_000,
+                    lat = _locationFlow.value!!.latitude,
+                    long = _locationFlow.value!!.longitude,
+                )
+                    .onSuccess { res ->
+                        _reports.value = res
+                        Log.i(Tag.Home.name, res.toString())
+                    }
+                    .onFailure { e ->
+                        _error.value = e.message
+                        Log.e(Tag.Home.name, e.message.toString())
+                        _uiState.value = UiState.Base
+                    }
+            }
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        locationCallBack?.let {
+            fusedLocationProviderClient.removeLocationUpdates(it)
+        }
     }
 }
